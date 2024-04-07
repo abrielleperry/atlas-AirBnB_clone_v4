69b9a147171153aef115f43b7bb274d4889143c3
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-04-07 17:34:41 -0500

    missing closing >

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,24 +1,21 @@
 #!/usr/bin/node
+const checkedAmenities = {};
 $(document).ready(function () {
-  // This function executes when the DOM is fully loaded
-
-  // Initialize an object to store checked amenity IDs
-  var checkedAmenities = {};
-
   // Listen for changes on each input checkbox
-  $('input[type="checkbox"]').change(function () {
+  $('li input[type="checkbox"]').on("change", function () {
     // This function executes when the state of a checkbox changes
 
     // Get the Amenity ID from the data-id attribute of the checkbox's parent <li> tag
     var amenityID = $(this).closest("li").data("id");
+    // Get the Amenity Name from the data-name attribute of the checkbox's parent <li> tag
+    var amenityName = $(this).closest("li").data("name");
 
-    // Check if the checkbox is checked
-    if ($(this).is(":checked")) {
-      // If checked, add the Amenity ID to the checkedAmenities object
-      checkedAmenities[amenityID] = true;
+    // If the amenity name is already in the list, remove it
+    if (checkedAmenities.hasOwnProperty(amenityName)) {
+      delete checkedAmenities[amenityName];
+      // Otherwise, add the amenity name and its ID to the object
     } else {
-      // If unchecked, remove the Amenity ID from the checkedAmenities object
-      delete checkedAmenities[amenityID];
+      checkedAmenities[amenityName] = amenityID;
     }
 
     // Update the h4 tag inside the div with id "amenities"
@@ -28,12 +25,7 @@ $(document).ready(function () {
   // Function to update the h4 tag inside the div with id "amenities"
   function updateAmenitiesList() {
     // Get the keys (Amenity IDs) from the checkedAmenities object
-    var amenityIDs = Object.keys(checkedAmenities);
-
-    // Get the names of amenities corresponding to the checked Amenity IDs
-    var amenityNames = amenityIDs.map(function (id) {
-      return $('li[data-id="' + id + '"]').data("name");
-    });
+    var amenityNames = Object.keys(checkedAmenities);
 
     // Update the text inside the h4 tag with the list of checked amenities separated by commas
     $("div#amenities > h4").text(amenityNames.join(", "));
