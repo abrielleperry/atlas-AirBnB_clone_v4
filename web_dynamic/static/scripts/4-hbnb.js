a04d31f2aeb0e8637566269c11003670fcb9f51a
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-04-07 20:49:15 -0500

    changes to click button

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,50 +1,76 @@
 $(document).ready(function () {
-  // Initialize an object to store checked amenity IDs
-  var checkedAmenities = {};
+  const checkedAmenities = {};
 
-  // Listen for changes on each input checkbox
   $('input[type="checkbox"]').change(function () {
-    // This function executes when the state of a checkbox changes
+    const amenityId = $(this).data("id");
+    const amenityName = $(this).data("name");
 
-    // Get the Amenity ID from the data-id attribute of the checkbox's parent <li> tag
-    var amenityID = $(this).closest("li").data("id");
-
-    // Check if the checkbox is checked
-    if ($(this).is(":checked")) {
-      // If checked, add the Amenity ID to the checkedAmenities object
-      checkedAmenities[amenityID] = true;
+    if (this.checked) {
+      checkedAmenities[amenityId] = amenityName;
     } else {
-      // If unchecked, remove the Amenity ID from the checkedAmenities object
-      delete checkedAmenities[amenityID];
+      delete checkedAmenities[amenityId];
     }
-  });
 
-  // Function to make a POST request to places_search with checked amenities
-  $("#filterButton").click(function () {
-    // Make a POST request to places_search with the list of checked amenities
-    $.ajax({
-      type: "POST",
-      url: "http://0.0.0.0:5001/api/v1/places_search",
-      contentType: "application/json",
-      data: JSON.stringify({ amenities: Object.keys(checkedAmenities) }),
-      success: function (response) {
-        // Clear existing places before adding filtered places
-        $("section.places").empty();
-
-        // Loop through the response and create article tags for each place
-        response.forEach(function (place) {
-          var article = $("<article>");
-          var name = $("<h2>").addClass("title_box").text(place.name);
-          article.append(name);
-          var description = $("<p>").text(place.description);
-          article.append(description);
-          $("section.places").append(article);
-        });
-      },
-      error: function (xhr, status, error) {
-        // Log any errors to the console
-        console.error("Error:", error);
-      },
-    });
+    const amenitiesList = Object.values(checkedAmenities).join(", ");
+    $(".amenities > h4").text(amenitiesList);
   });
 });
+
+// Task 3: Check API status
+function checkAPIStatus() {
+  $.get(
+    "http://http://0.0.0.0:5001/api/v1/status/",
+    { type: "GET" },
+    function (data) {
+      if (data.status === "OK") {
+        $("#api_status").addClass("available");
+      } else {
+        $("#api_status").removeClass("available");
+      }
+    }
+  );
+}
+
+// Task 4: Fetch places from the API
+function placesSearch() {
+  const checkedAmenities = {};
+  $('input[type="checkbox"]:checked').each(function () {
+    const amenityId = $(this).data("id");
+    const amenityName = $(this).data("name");
+    checkedAmenities[amenityId] = amenityName;
+  });
+
+  const dataPayload = {
+    amenities: Object.keys(checkedAmenities),
+  };
+
+  $.ajax({
+    url: "http://0.0.0.0:5001/api/v1/places_search",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(checkedAmenities),
+    dataType: "json",
+    success: function (response) {
+      $("section.places").empty();
+      response.map((place) => {
+        $("section.places").append(
+          `<article>
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest(s)</div>
+              <div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
+              <div class="number_bathrooms">
+                ${place.number_bathrooms} Bathroom(s)
+              </div>
+            </div>
+            <div class="description">${place.description}</div>
+          </article>`
+        );
+      });
+    },
+  });
+}
+$("#search").click(placesSearch);
