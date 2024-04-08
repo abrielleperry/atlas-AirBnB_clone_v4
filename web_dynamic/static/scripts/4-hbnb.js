650c5d133d0ce69b44f92d284a96adb647192b1d
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-04-08 11:44:47 -0500

    simplify order

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,6 +1,40 @@
 $(document).ready(function () {
   const checkedAmenities = {};
 
+  function updateAmenitiesList() {
+    const amenitiesList = Object.values(checkedAmenities).join(", ");
+    $(".amenities > h4").text(amenitiesList);
+  }
+
+  function performSearch() {
+    $.ajax({
+      url: "http://0.0.0.0:5001/api/v1/places_search",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(checkedAmenities),
+      dataType: "json",
+      success: function (response) {
+        $("section.places").empty();
+        response.map((place) => {
+          $("section.places").append(`
+            <article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">${place.max_guest} Guest(s)</div>
+                <div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
+                <div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>
+              </div>
+              <div class="description">${place.description}</div>
+            </article>
+          `);
+        });
+      },
+    });
+  }
+
   $('input[type="checkbox"]').change(function () {
     const amenityId = $(this).data("id");
     const amenityName = $(this).data("name");
@@ -11,66 +45,8 @@ $(document).ready(function () {
       delete checkedAmenities[amenityId];
     }
 
-    const amenitiesList = Object.values(checkedAmenities).join(", ");
-    $(".amenities > h4").text(amenitiesList);
+    updateAmenitiesList();
   });
+
+  $("#search").click(performSearch);
 });
-
-// Task 3: Check API status
-function checkAPIStatus() {
-  $.get(
-    "http://http://0.0.0.0:5001/api/v1/status/",
-    { type: "GET" },
-    function (data) {
-      if (data.status === "OK") {
-        $("#api_status").addClass("available");
-      } else {
-        $("#api_status").removeClass("available");
-      }
-    }
-  );
-}
-
-// Task 4: Fetch places from the API
-function placesSearch() {
-  const checkedAmenities = {};
-  $('input[type="checkbox"]:checked').each(function () {
-    const amenityId = $(this).data("id");
-    const amenityName = $(this).data("name");
-    checkedAmenities[amenityId] = amenityName;
-  });
-
-  const dataPayload = {
-    amenities: Object.keys(checkedAmenities),
-  };
-
-  $.ajax({
-    url: "http://0.0.0.0:5001/api/v1/places_search",
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify(checkedAmenities),
-    dataType: "json",
-    success: function (response) {
-      $("section.places").empty();
-      response.map((place) => {
-        $("section.places").append(
-          `<article>
-            <div class="title_box">
-              <h2>${place.name}</h2>
-              <div class="price_by_night">${place.price_by_night}</div>
-            </div>
-            <div class="information">
-              <div class="max_guest">${place.max_guest} Guest(s)</div>
-              <div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
-              <div class="number_bathrooms">
-                ${place.number_bathrooms} Bathroom(s)
-              </div>
-            </div>
-            <div class="description">${place.description}</div>
-          </article>`
-        );
-      });
-    },
-  });
-}
-$("#search").click(placesSearch);
